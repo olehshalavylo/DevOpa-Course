914754da6f5e65d2e4964c4ef91b4d25fd6045ee
olehshalavylo <61602366+olehshalavylo@users.noreply.github.com>
2021-12-11 23:06:19 +0200

    Add files via upload

---
+++ Hometask3/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo find / -type -l
+
+sudo sed -n '/^Character/, /^$/ { /^$/ !p }' /proc/devices
+sudo sed -n '/^Block/, /^$/ { /^$/ !p }' /proc/devices
+
+sudo find . \! -perm /1000
+
+sudo ln -s /etc/hostname /tmp
+
+sudo useradd testuser
+
+sudo touch /home/testuser_data && sudo chown testuser:testuser /home/testuser_data
